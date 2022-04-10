c2d58c3ee768cef2feaf521a2415ae13d29ba608
lmorais <leozonnn@gmail.com>
2022-04-10 15:03:37 -0300

    test: validate domain emails with domain parts larger than 63 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -48,4 +48,10 @@ describe("Email validation", () => {
 
     expect(Email.validate(email)).toBeFalsy();
   });
+
+  it("should not accept domain with a part larger than 63 chars", () => {
+    const email = `any@${"d".repeat(64)}.com`;
+
+    expect(Email.validate(email)).toBeFalsy();
+  });
 });
