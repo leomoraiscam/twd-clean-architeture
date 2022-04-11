1250635cb8ee7bfe444d67f6cb62543cc8764440
lmorais <leozonnn@gmail.com>
2022-04-11 15:16:07 -0300

    chore: add jest intregation test config

---
+++ jest-intregation-config.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line import/extensions
+const config = require("./jest.config");
+
+config.testMatch = ["**/*.test.ts"];
+
+module.exports = config;
