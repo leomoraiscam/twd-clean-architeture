217bd77865707e0edcdf8abac81ba2dbe7d5d4b9
lmorais <leozonnn@gmail.com>
2022-04-10 23:14:40 -0300

    test: verify eamil address when creating user

---
+++ src/entities/user.spec.ts
@@ -0,0 +1,16 @@
+import { left } from "../shared/either";
+import User from "./user";
+import InvalidEmailError from "./errors/invalid-email-error";
+
+describe("User domain class", () => {
+  it("should not create user with invalid email address", () => {
+    const invalidEmail = "invalid_email";
+
+    const error = User.create({
+      name: "any_name",
+      email: invalidEmail,
+    });
+
+    expect(error).toEqual(left(new InvalidEmailError()));
+  });
+});
