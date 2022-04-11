b8cee2085fd48dcde90cc93fddb66032f561b1c4
lmorais <leozonnn@gmail.com>
2022-04-11 15:12:54 -0300

    chore: improve jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ["<rootDir>/src"],
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  testEnvironment: 'node',
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!**/test/**",
+    "!**/config/**",
+  ],
+  testEnvironment: "node",
   transform: {
-    '.+\\.ts$': 'ts-jest',
+    ".+\\.ts$": "ts-jest",
   },
 };
