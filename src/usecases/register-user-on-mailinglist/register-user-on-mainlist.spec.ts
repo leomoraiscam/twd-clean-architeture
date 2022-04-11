32d570684d0b08433a5e250f61793282b0a91493
lmorais <leozonnn@gmail.com>
2022-04-11 15:50:18 -0300

    test: register valid user to mainling list

--- src/usecases/register-user-on-mailinglist/register-user-on-mainlist.spec.ts
+++ src/usecases/register-user-on-mailinglist/register-user-on-mainlist.spec.ts
@@ -1,5 +1,7 @@
 import InMemoryUserRepository from "./repositories/in-memory-user-repository";
 import UserData from "../../entities/user-data";
+import RegisterUserOnMainList from "./register-user-on-mainlist";
+import UserRepository from "./ports/user-repository";
 
 describe("Register user on main list use case", () => {
   it("should add user with complete data to mainling list", async () => {
@@ -7,17 +9,38 @@ describe("Register user on main list use case", () => {
 
     const userRepository: UserRepository = new InMemoryUserRepository(users);
 
-    const usecase: RegisterUserOnMainlist = new RegisterUserOnMainlist(
+    const usecase: RegisterUserOnMainList = new RegisterUserOnMainList(
       userRepository
     );
 
     const name = "Rachel Klein";
     const email = "lutefde@kipijaw.fj";
 
-    const response = await usecase.RegisterUserOnMainlist({ name, email });
+    const response = await usecase.registerUserOnMainlist({ name, email });
 
     const user = userRepository.findUserByEmail("lutefde@kipijaw.fj");
 
     expect((await user).name).toBe("Rachel Klein");
+    expect(response.value.name).toBe("Rachel Klein");
+  });
+
+  it.skip("should not add user with invalid email", async () => {
+    const users: UserData[] = [];
+
+    const userRepository: UserRepository = new InMemoryUserRepository(users);
+
+    const usecase: RegisterUserOnMainList = new RegisterUserOnMainList(
+      userRepository
+    );
+
+    const name = "Rachel Klein";
+    const email = "lutefde@kipijaw.fj";
+
+    const response = await usecase.registerUserOnMainlist({ name, email });
+
+    const user = userRepository.findUserByEmail("lutefde@kipijaw.fj");
+
+    expect((await user).name).toBe("Rachel Klein");
+    expect(response.value.name).toBe("Rachel Klein");
   });
 });
