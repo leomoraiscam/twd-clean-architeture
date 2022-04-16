013fec81d503fba84996778af05c9fbf1840e205
lmorais <leozonnn@gmail.com>
2022-04-16 16:30:07 -0300

    test: web controller should return 400 when user has invalid email ou name

--- tests/web-controllers/sign-up-controller.spec.ts
+++ tests/web-controllers/sign-up-controller.spec.ts
@@ -1,8 +1,10 @@
 import { UserData } from "@/entities";
+import { InvalidEmailError, InvalidNameError } from "@/entities/errors";
 import RegisterUserOnMainList from "@/usecases/register-user-on-mailinglist/register-user-on-mainlist";
 import InMemoryUserRepository from "@/usecases/register-user-on-mailinglist/repositories/in-memory-user-repository";
 import { HttpRequest, HttpResponse } from "@/web-controllers/ports";
 import { RegisterUserController } from "@/web-controllers/register-user-controller";
+import { MissingParamError } from "@/web-controllers/errors";
 
 describe("Sign Up web controller", () => {
   it("should return status code 201 when request contains valid user data", async () => {
@@ -27,11 +29,11 @@ describe("Sign Up web controller", () => {
     expect(response.body).toEqual(request.body);
   });
 
-  it.skip("should return status code 201 when request contains valid user data", async () => {
-    const request: HttpRequest = {
+  it("should return status code 400 when request contains invalid name", async () => {
+    const requestWithInvalidName: HttpRequest = {
       body: {
-        name: "Larry Gonzales",
-        email: "avtu@kodo.vn",
+        name: "L",
+        email: "baavo@duzas.re",
       },
     };
 
@@ -43,9 +45,59 @@ describe("Sign Up web controller", () => {
     const controller: RegisterUserController = new RegisterUserController(
       usecase
     );
-    const response: HttpResponse = await controller.handle(request);
+    const response: HttpResponse = await controller.handle(
+      requestWithInvalidName
+    );
 
-    expect(response.statusCode).toEqual(201);
-    expect(response.body).toEqual(request.body);
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toBeInstanceOf(InvalidNameError);
+  });
+
+  it("should return status code 400 when request contains invalid email", async () => {
+    const requestWithInvalidEmail: HttpRequest = {
+      body: {
+        name: "Eula Peters",
+        email: "giagadocvikto.br",
+      },
+    };
+
+    const users: UserData[] = [];
+    const userRepository = new InMemoryUserRepository(users);
+    const usecase: RegisterUserOnMainList = new RegisterUserOnMainList(
+      userRepository
+    );
+    const controller: RegisterUserController = new RegisterUserController(
+      usecase
+    );
+    const response: HttpResponse = await controller.handle(
+      requestWithInvalidEmail
+    );
+
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toBeInstanceOf(InvalidEmailError);
+  });
+
+  it.skip("should return status code 400 when request is missing user name", async () => {
+    const requestWithoutName: HttpRequest = {
+      body: {
+        email: "Maurice Page",
+      },
+    };
+
+    const users: UserData[] = [];
+    const userRepository = new InMemoryUserRepository(users);
+    const usecase: RegisterUserOnMainList = new RegisterUserOnMainList(
+      userRepository
+    );
+    const controller: RegisterUserController = new RegisterUserController(
+      usecase
+    );
+    const response: HttpResponse = await controller.handle(requestWithoutName);
+
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toBeInstanceOf(MissingParamError);
+    expect(response.body as Error).toEqual(
+      "Missing parameter from request: name."
+    );
   });
 });
