bfa10d2fe523e9283691a50ea8977a07e90578f4
lmorais <leozonnn@gmail.com>
2022-04-17 00:06:57 -0300

    test: apply body parser middleware json

---
+++ tests/main/middleware/body-parse.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest';
+
+import app from '@/main/config/app';
+
+describe('Body parser middleware', () => {
+  it('Should be able parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body);
+    });
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({
+        name: 'Viola Barton',
+      })
+      .expect({
+        name: 'Viola Barton',
+      });
+  });
+});
