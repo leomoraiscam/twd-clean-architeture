f3728c80234803faa4da21d71bac92320f19b7a7
lmorais <leozonnn@gmail.com>
2022-04-16 23:35:58 -0300

    chore: install express

--- src/web-controllers/errors/missing-param-error.ts
+++ src/web-controllers/errors/missing-param-error.ts
@@ -1,5 +1,5 @@
 export class MissingParamError extends Error {
-  public readonly name = "MissingParamError";
+  public readonly name = 'MissingParamError';
 
   constructor(param: string) {
     super(`Missing parameter from request: ${param}.`);
