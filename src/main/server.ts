45c13694d5d2911db6428e4d83f339b05d6dcc3c
lmorais <leozonnn@gmail.com>
2022-04-20 18:38:01 -0300

    feat: connect to mongodb before listening

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,15 @@
 /* eslint-disable no-console */
 import 'module-alias/register';
-import app from '@/main/config/app';
+import { MongoHelper } from '@/external/repositories/mongodb/helper/index';
 
-app.listen(5000, () => {
-  console.log('Server running at http://localhost:5000');
-});
+MongoHelper.connect(process.env.MONGO_URL)
+  .then(async () => {
+    const app = (await import('./config/app')).default;
+
+    app.listen(5000, () => {
+      console.log('Server running at http://localhost:5000');
+    });
+  })
+  .catch((error) => {
+    console.log(`Error: ${error}`);
+  });
