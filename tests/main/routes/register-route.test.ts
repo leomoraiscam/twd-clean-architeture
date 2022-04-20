a9ba7f51857f388dda8ffe9e78e7547c761c4bb6
lmorais <leozonnn@gmail.com>
2022-04-20 19:00:56 -0300

    test: connect and disconect in intregartion test

--- tests/main/routes/register-route.test.ts
+++ tests/main/routes/register-route.test.ts
@@ -1,8 +1,21 @@
 import request from 'supertest';
 
+import { MongoHelper } from '@/external/repositories/mongodb/helper';
 import app from '@/main/config/app';
 
 describe('Register router', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    MongoHelper.clearCollection('users');
+  });
+
   it('Should return be able an account on success', async () => {
     app.post('/test_cors', (req, res) => {
       res.send();
